74bbe9537bf63b8aee8e19d9534d6ee23980b4d4
ezzp024 <ezzp024@users.noreply.github.com>
2026-03-24 16:07:47 -0700

    Configure Google client ID for frontend sign-in

--- config.js
+++ config.js
@@ -1,2 +1,4 @@
 window.FORUM_API_BASE = window.FORUM_API_BASE || 'http://localhost:8787/api';
-window.GOOGLE_CLIENT_ID = window.GOOGLE_CLIENT_ID || '';
+window.GOOGLE_CLIENT_ID =
+  window.GOOGLE_CLIENT_ID ||
+  '923128845782-t4j1gf0leul3qejnk4mp5h3ngbe9acb1.apps.googleusercontent.com';
